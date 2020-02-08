8e0aaf0f69211ffa85e6e8f6560fb87201dff399
jmargielewski <kuba.margielewski@gmail.com>
2020-02-08 21:43:59 +0000

    fetch and list products

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
-function Home() {
-  return <>home</>;
+import React from "react";
+import axios from "axios";
+import ProductList from "../components/Index/ProductList";
+
+function Home({ products }) {
+  return <ProductList products={products} />;
 }
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const url = "http://localhost:3000/api/products";
+  const response = await axios.get(url);
+
+  // return data as an object
+  // note: this object will be merged with existing props
+  return { products: response.data };
+};
 
 export default Home;
