d00dca18f0c6ff517fc7345cbf1297733ffdaeb7
jmargielewski <kuba.margielewski@gmail.com>
2020-02-12 20:52:48 +0000

    product page

--- pages/product.js
+++ pages/product.js
@@ -1,5 +1,26 @@
-function Product() {
-  return <>product</>;
+import axios from "axios";
+import ProductSummary from "../components/Product/ProductSummary";
+import ProductAttributes from "../components/Product/ProductAttributes";
+import baseUrl from "../utils/baseUrl";
+
+function Product({ product }) {
+  return (
+    <>
+      <ProductSummary {...product} />
+      <ProductAttributes {...product} />
+    </>
+  );
 }
+
+Product.getInitialProps = async ({ query: { _id } }) => {
+  // fetch data on server
+  const url = `${baseUrl}/api/product`;
+  const payload = { params: { _id } };
+  const response = await axios.get(url, payload);
+
+  // return data as an object
+  // note: this object will be merged with existing props
+  return { product: response.data };
+};
 
 export default Product;
