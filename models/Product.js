0646edcf5da3a9f2fd75ad8a8cf615a7da491ab8
jmargielewski <kuba.margielewski@gmail.com>
2020-02-12 20:48:45 +0000

    fix sku value

--- models/Product.js
+++ models/Product.js
@@ -19,7 +19,7 @@ const ProductSchema = new mongoose.Schema({
   sku: {
     type: String,
     unique: true,
-    default: shortid()
+    default: shortid.generate()
   },
   mediaUrl: {
     type: String,
