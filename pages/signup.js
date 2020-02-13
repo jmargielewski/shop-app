200dacddf5a8da2569c2e5bdc39101d4d6c2547e
jmargielewski <kuba.margielewski@gmail.com>
2020-02-13 21:44:35 +0000

    add signup form

--- pages/signup.js
+++ pages/signup.js
@@ -1,7 +1,44 @@
+import React from "react";
 import { Button, Form, Icon, Message, Segment } from "semantic-ui-react";
 import Link from "next/link";
+import catchError from "../utils/catchErrors";
+
+const INITIAL_USER = {
+  name: "",
+  email: "",
+  password: ""
+};
 
 function Signup() {
+  const [user, setUser] = React.useState(INITIAL_USER);
+  const [disabled, setDisabled] = React.useState(true);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState("");
+
+  React.useEffect(() => {
+    const isUser = Object.values(user).every(el => Boolean(el));
+    isUser ? setDisabled(false) : setDisabled(true);
+  }, [user]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setUser(prevState => ({ ...prevState, [name]: value }));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      setLoading(true);
+      setError("");
+      console.log(user);
+    } catch (error) {
+      catchError(error, setError);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <>
       <Message
@@ -11,7 +48,8 @@ function Signup() {
         content="Create a new account"
         color="teal"
       />
-      <Form>
+      <Form error={Boolean(error)} loading={loading} onSubmit={handleSubmit}>
+        <Message error header="Oops!" content={error} />
         <Segment>
           <Form.Input
             fluid
@@ -20,14 +58,19 @@ function Signup() {
             label="Name"
             placeholder="Name"
             name="name"
+            value={user.name}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
-            icon="envelop"
+            icon="envelope"
             iconPosition="left"
             label="Email"
             placeholder="Email"
             name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
@@ -36,8 +79,17 @@ function Signup() {
             label="Password"
             placeholder="Password"
             name="password"
+            type="password"
+            value={user.password}
+            onChange={handleChange}
           />
-          <Button icon="signup" type="submit" color="orange" content="Signup" />
+          <Button
+            disabled={disabled || loading}
+            icon="signup"
+            type="submit"
+            color="orange"
+            content="Signup"
+          />
         </Segment>
       </Form>
       <Message attached="bottom" warning>
