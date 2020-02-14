9e41aece8a5e714ed844eab17f4476f4b032a620
jmargielewski <kuba.margielewski@gmail.com>
2020-02-14 20:58:52 +0000

    create new cart for user

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,9 +1,10 @@
 import User from "../../models/User";
+import Cart from "../../models/Cart";
 import connectDb from "../../utils/connectDb";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import isEmail from 'validator/lib/isEmail';
-import isLength from 'validator/lib/isLength';
+import isEmail from "validator/lib/isEmail";
+import isLength from "validator/lib/isLength";
 
 connectDb();
 
@@ -13,11 +14,13 @@ export default async (req, res) => {
   try {
     // 1) Validate name / email / password
     if (!isLength(name, { min: 3, max: 10 })) {
-      return res.status(422).send('Name must be 3-10 characters long');
+      return res.status(422).send("Name must be 3-10 characters long");
     } else if (!isLength(password, { min: 6 })) {
-      return res.status(422).send('Password must be at least 6 characters long');
+      return res
+        .status(422)
+        .send("Password must be at least 6 characters long");
     } else if (!isEmail(email)) {
-      return res.status(422).send('Email must be valid');
+      return res.status(422).send("Email must be valid");
     }
 
     // 2) Check to see if the user already exist in the db
@@ -36,12 +39,14 @@ export default async (req, res) => {
       password: hash
     }).save();
 
-    // 5) create token for the new user
+    // 5) create cart for new user
+    await new Cart({ user: newUser._id }).save();
+    // 6) create token for the new user
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: "7d"
     });
 
-    // 6) send back token
+    // 7) send back token
     res.status(201).json(token);
   } catch (error) {
     console.error(error);
