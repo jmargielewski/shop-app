847e809e05e5a78dc4b45b11ec2a8b64f23d5c2a
jmargielewski <kuba.margielewski@gmail.com>
2020-02-16 16:31:07 +0000

    cart summary

--- components/Cart/CartSummary.js
+++ components/Cart/CartSummary.js
@@ -1,12 +1,30 @@
+import React from "react";
 import { Segment, Button, Divider } from "semantic-ui-react";
+import calculateCartTotal from "../../utils/calculateCartTotal";
 
-function CartSummary() {
+function CartSummary({ products }) {
+  const [cartAmount, setCartAmount] = React.useState(0);
+  const [stripeAmount, setStripeAmount] = React.useState(0);
+  const [isCartEmpty, setCartEmpty] = React.useState(false);
+
+  React.useEffect(() => {
+    const { cartTotal, stripeTotal } = calculateCartTotal(products);
+    setCartAmount(cartTotal);
+    setStripeAmount(stripeTotal);
+    setCartEmpty(products.length === 0);
+  }, [products]);
   return (
     <>
       <Divider />
       <Segment clearing size="large">
-        <strong>Sub total:</strong> $0.00
-        <Button icon="cart" color="teal" floated="right" content="checkout" />
+        <strong>Sub total:</strong> ${cartAmount}
+        <Button
+          disabled={isCartEmpty}
+          icon="cart"
+          color="teal"
+          floated="right"
+          content="checkout"
+        />
       </Segment>
     </>
   );
