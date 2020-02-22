30182060e113249fa57a8f1ca6c42a14b922568c
jmargielewski <kuba.margielewski@gmail.com>
2020-02-22 18:32:34 +0000

    fix key

--- components/Account/AccountOrders.js
+++ components/Account/AccountOrders.js
@@ -34,7 +34,7 @@ function AccountOrders({ orders }) {
               />
               <List>
                 {order.products.map(p => (
-                  <List.Item key={p._id}>
+                  <List.Item key={p.product._id}>
                     <Image avatar src={p.product.mediaUrl} />
                     <List.Content>
                       <List.Header>{p.product.name}</List.Header>
