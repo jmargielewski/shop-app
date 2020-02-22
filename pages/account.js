b9cab18929ca32f83df87a04fc811405d91cd7ab
jmargielewski <kuba.margielewski@gmail.com>
2020-02-22 16:15:45 +0000

    fetch orders

--- pages/account.js
+++ pages/account.js
@@ -1,5 +1,26 @@
-function Account() {
-  return <>account</>;
+import axios from "axios";
+import AccountHeader from "../components/Account/AccountHeader";
+import AccountOrders from "../components/Account/AccountOrders";
+import { parseCookies } from "nookies";
+import baseUrl from "../utils/baseUrl";
+
+function Account({ user, orders }) {
+  return (
+    <>
+      <AccountHeader {...user} />
+      <AccountOrders orders={orders} />
+    </>
+  );
 }
+Account.getInitialProps = async ctx => {
+  const { token } = parseCookies(ctx);
+  if (!token) {
+    return { order: [] };
+  }
+  const payload = { headers: { Authorization: token } };
+  const url = `${baseUrl}/api/orders`;
+  const response = await axios.get(url, payload);
+  return response.data;
+};
 
 export default Account;
